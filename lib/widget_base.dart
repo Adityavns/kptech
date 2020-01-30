39c75e03ed434d3d165cb8013c95695e92768c9f
Aditya VNS <vns.aditya@gmail.com>
2020-01-31 02:22:54 +0530

    add firebase

---
+++ lib/widget_base.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class WidgetBase extends StatelessWidget {
+  final Widget child;
+
+  const WidgetBase(this.child) : super();
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
