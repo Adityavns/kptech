7cb64da885a4d392693bbf791b2cb866873e287c
Varuhn <varunsachin1010@gmail.com>
2020-01-31 02:42:53 +0530

    text field

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:kptech/widget_base.dart';
 
 void main() => runApp(MyApp());
 
@@ -28,28 +29,48 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
-    return new Scaffold(body: getBody());
+    return new Scaffold(
+      body: getBody(),
+      appBar: AppBar(
+        title: Text("KP Tech"),
+      ),
+    );
   }
 
   Widget getBody() {
-    return Column(
-      children: <Widget>[
-        widget1(),
-        widget2(),
-        widget3(),
-      ],
+    return SingleChildScrollView(
+      child: Column(
+        children: <Widget>[
+          widget1(),
+          widget2(),
+          widget3(),
+        ],
+      ),
     );
   }
 
   Widget widget1() {
-    return new Text("");
+    return WidgetBase(child: Text("Pranay"));
   }
 
   Widget widget2() {
-    return new Text("");
+    return Column(
+      children: <Widget>[],
+    );
   }
 
   Widget widget3() {
-    return new Text("");
+    return Text("Jashu");
+  }
+
+  Widget getTextField(
+      TextEditingController controller, int maxLength, String hintText) {
+    return TextFormField(
+      controller: controller,
+      maxLength: maxLength,
+      decoration: InputDecoration(
+        hintText: hintText,
+      ),
+    );
   }
 }
