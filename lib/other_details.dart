4901f9e08af23e5b38440fd44acb39befd1b95fc
Varuhn <varunsachin1010@gmail.com>
2020-01-31 20:12:07 +0530

    other details

--- lib/other_details.dart
+++ lib/other_details.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:kptech/communication_details.dart';
 
 class OtherDetails extends StatefulWidget {
-  Map map;
+  final Map map;
   @override
   OtherDetails(this.map) {
     print(map);
@@ -25,8 +25,6 @@ class _OtherDetailsState extends State<OtherDetails> {
   final permanentAddressTehsil = TextEditingController();
   final permanentAddressConstituency = TextEditingController();
   final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
-  String dropDownValue = "One";
-  bool isAdhar = false;
 
   @override
   Widget build(BuildContext context) {
@@ -104,6 +102,17 @@ class _OtherDetailsState extends State<OtherDetails> {
           padding: const EdgeInsets.all(8.0),
           child: TextFormField(
             validator: (val) => val.isEmpty ? "Field cannot be empty" : null,
+            controller: educationLevel,
+            decoration: InputDecoration(
+              border: OutlineInputBorder(),
+              labelText: "Educational Qualification",
+            ),
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: TextFormField(
+            validator: (val) => val.isEmpty ? "Field cannot be empty" : null,
             controller: permanentAddress,
             decoration: InputDecoration(
               border: OutlineInputBorder(),
@@ -130,6 +139,17 @@ class _OtherDetailsState extends State<OtherDetails> {
             decoration: InputDecoration(
               border: OutlineInputBorder(),
               labelText: "Permanent address District",
+            ),
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: TextFormField(
+            validator: (val) => val.isEmpty ? "Field cannot be empty" : null,
+            controller: permanentAddressPinCode,
+            decoration: InputDecoration(
+              border: OutlineInputBorder(),
+              labelText: "Permanent address Pin Code",
             ),
           ),
         ),
@@ -198,14 +218,15 @@ class _OtherDetailsState extends State<OtherDetails> {
     map["adharNo"] = adharNo.text;
     map["countryCode"] = countryCode.text;
     map["mobileNo"] = mobileNo.text;
+    map["educationLevel"] = educationLevel.text;
     map["permanentAddress"] = permanentAddress.text;
     map["permanentAddressState"] = permanentAddressState.text;
     map["permanentAddressDistrict"] = permanentAddressDistrict.text;
+    map["permanentAddressPinCode"] = permanentAddressPinCode.text;
     map["permanentAddressCity"] = permanentAddressCity.text;
     map["permanentAddressTehsil"] = permanentAddressTehsil.text;
     map["permanentAddressConstituency"] = permanentAddressConstituency.text;
     print(map);
     return map;
-
   }
 }
